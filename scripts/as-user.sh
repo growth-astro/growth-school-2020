18ecaf955fcbcaee9488af095aa8b7062ffc4c74
David Kaplan <kaplan@uwm.edu>
2020-07-27 13:38:37 -0500

    Including creation of desktop icons

--- scripts/as-user.sh
+++ scripts/as-user.sh
@@ -19,5 +19,25 @@ initialize_conda() {
     fi
 }
 
+initialize_desktop() {
+    echo "initialize desktop()"
+    # add in some desktop icons
+    FILE="/nfs/misc/DS9.desktop"
+    DEST="/home/$NB_USER/Desktop/DS9.desktop"
+    if test -d "/home/$NB_USER/Desktop/"; then
+      echo "/home/$NB_USER/Desktop/ exists"
+    else
+      echo "Creating /home/$NB_USER/Desktop/"
+      mkdir /home/$NB_USER/Desktop/
+    fi
+    if test -f $DEST; then
+      echo "$DEST exists, will not alter it"
+    else
+      cp $FILE $DEST
+      echo "Copied $FILE to $DEST"
+    fi
+}
+
 get_notebooks
 initialize_conda
+initialize_desktop
